babb60969bded4bbd076a940220332eab3761479
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-11-28 23:27:46 +0700

    :construction: working on AdvanceSearchBox ui

--- src/views/SearchInfo/AdvanceSearchBox.jsx
+++ src/views/SearchInfo/AdvanceSearchBox.jsx
@@ -1,59 +1,78 @@
-import { Box, Collapse, Grid, TextField, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Box, Button, Collapse, Grid, Link, makeStyles, TextField, Typography } from "@material-ui/core";
+import clsx from "clsx";
+import { useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  marginBottomChild: {
+    "& > *": {
+      marginBottom: theme.spacing(2),
+      "&:last-child": {
+        marginBottom: 0,
+      },
+    },
+  },
+}));
 
 export default function AdvanceSearchBox(props) {
+  const cls = useStyles();
+  const [open, setOpen] = useState(false);
+
   return (
-    <Box>
-      <Typography align="right">
-        <Link>Tìm kiếm nâng cao</Link>
-      </Typography>
-      <Collapse in={true}>
-        <Box>
-          <Typography>Lưu ý: Các giá trị ngăn cách nhau bởi dấu phẩy.</Typography>
-        </Box>
-        <Box>
-          <Grid container>
-            <Grid md item>
-              Time
+    <Box my={2}>
+      <Box textAlign="right" mb={1}>
+        <Button color="primary" onClick={(e) => setOpen(!open)}>
+          Tìm kiếm nâng cao
+        </Button>
+      </Box>
+      <Collapse in={open}>
+        <Box p={2} bgcolor="white">
+          <Box mb={3}>
+            <Typography>Lưu ý: Các giá trị ngăn cách nhau bởi dấu phẩy.</Typography>
+          </Box>
+          <Box className={cls.marginBottomChild}>
+            <Grid container spacing={4} alignItems="center">
+              <Grid md={1} item>
+                <Typography variant="button">Time:</Typography>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> A || B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> A && B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> !A !B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
             </Grid>
-            <Grid md item>
-              <TextField label="OR" fullWidth></TextField>
+            <Grid container spacing={4} alignItems="center">
+              <Grid md={1} item>
+                <Typography variant="button">Link:</Typography>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> A || B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> A && B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> !A !B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
             </Grid>
-            <Grid md item>
-              <TextField label="AND" fullWidth></TextField>
+            <Grid container spacing={4} alignItems="center">
+              <Grid md={1} item>
+                <Typography variant="button">Content:</Typography>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> A || B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> A && B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
+              <Grid md item>
+                <TextField label="A, B --> !A !B" fullWidth InputLabelProps={{ shrink: true }}></TextField>
+              </Grid>
             </Grid>
-            <Grid md item>
-              <TextField label="NOT" fullWidth></TextField>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid md item>
-              Link
-            </Grid>
-            <Grid md item>
-              <TextField label="OR" fullWidth></TextField>
-            </Grid>
-            <Grid md item>
-              <TextField label="AND" fullWidth></TextField>
-            </Grid>
-            <Grid md item>
-              <TextField label="NOT" fullWidth></TextField>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid md item>
-              Content
-            </Grid>
-            <Grid md item>
-              <TextField label="OR" fullWidth></TextField>
-            </Grid>
-            <Grid md item>
-              <TextField label="AND" fullWidth></TextField>
-            </Grid>
-            <Grid md item>
-              <TextField label="NOT" fullWidth></TextField>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
       </Collapse>
     </Box>
--- src/views/SearchInfo/index.js
+++ src/views/SearchInfo/index.js
@@ -2,12 +2,14 @@ import SearchBar from "./SearchBar";
 import View from "../../utils/View";
 import ResultItem from "./ResultItem";
 import ResultList from "./ResultList";
+import AdvanceSearchBox from "./AdvanceSearchBox";
 
 export default function SearchInfo(props) {
   return (
     <div>
       <View title="Tìm kiếm thông tin">
         <SearchBar></SearchBar>
+        <AdvanceSearchBox></AdvanceSearchBox>
         <ResultList></ResultList>
       </View>
     </div>
