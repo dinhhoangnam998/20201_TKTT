75615c54f76f1187ed3eea4af808fc8f4cfd517e
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-11-28 21:22:01 +0700

    :heavy_plus_sign: add perfect scroll bar

--- src/utils/View.js
+++ src/utils/View.js
@@ -1,13 +1,16 @@
 import { Box, Container } from "@material-ui/core";
 import Page from "./Page";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 export default function View({ children, title }) {
   return (
     <div>
       <Page title={title}>
-        <Container>
-          <Box py={3}>{children}</Box>
-        </Container>
+        <PerfectScrollbar style={{ height: "90vh" }}>
+          <Container style={{ overflow: "hidden" }}>
+            <Box py={3}>{children}</Box>
+          </Container>
+        </PerfectScrollbar>
       </Page>
     </div>
   );
