785adb0d5f9dd00dc94c00eb1ecbfe117845efcc
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-22 22:44:26 +0700

    add scroll to top when click pagination

--- src/utils/View.js
+++ src/utils/View.js
@@ -2,11 +2,19 @@ import { Box, Container } from "@material-ui/core";
 import Page from "./Page";
 import PerfectScrollbar from "react-perfect-scrollbar";
 
+let containerRef;
+export { containerRef };
+
 export default function View({ children, title }) {
   return (
     <div>
       <Page title={title}>
-        <PerfectScrollbar style={{ height: "90vh" }}>
+        <PerfectScrollbar
+          containerRef={(ref) => {
+            containerRef = ref;
+          }}
+          style={{ height: "90vh" }}
+        >
           <Container style={{ overflow: "hidden" }}>
             <Box py={3}>{children}</Box>
           </Container>
